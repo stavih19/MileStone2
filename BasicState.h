1908f795cd2620edc664dbf7b1a4f3d4fcc15416
stavih19 <stavih19@gmail.com>
2020-01-17 10:14:42 +0200

    save in Brancg

---
+++ BasicState.h
@@ -0,0 +1,14 @@
+//
+// Created by stavih19 on 17/01/2020.
+//
+
+#ifndef MILESTONE2_BASICSTATE_H
+#define MILESTONE2_BASICSTATE_H
+
+
+class BasicState {
+
+};
+
+
+#endif //MILESTONE2_BASICSTATE_H
